7ae459f820fd913d5381f2e8412915bad3f73e0c
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-02-23 18:20:02 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+
+  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ン一-龥]+\z/
+  validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー－]+\z/
+
 end
