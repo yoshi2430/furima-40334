f7fcdac5b5bbf041ec41823637d7df4ea33be97b
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-02-26 14:34:57 +0900

    テーブル作成・ActiveStorageを導入

--- Gemfile
+++ Gemfile
@@ -81,3 +81,5 @@ end
 
 gem'devise'
 
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :items
+  has_many :purchases
+  has_one_attached :avatar
+  
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
