4046cc2a93850b40e3504804e82f22bd868bb656
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-02-26 15:29:52 +0900

    商品詳細アクティブハッシュ導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+   belongs_to :category
+   belongs_to :Condition
+
+  
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
