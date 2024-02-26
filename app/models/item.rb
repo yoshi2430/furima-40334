d6a840e20782f8d67d34bc625b0a3ee899ce5db3
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-02-26 16:46:57 +0900

    配送についてアクティブハッシュ導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,14 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to :category
-   belongs_to :Condition
-
+   belongs_to :condition
+   belongs_to :shipping_fee_burden
+   belongs_to :prefecture
+   belongs_to :shipping_day
   
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_burden_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
