a5046f757a681eb9a8a5e78a9b1196ac3ee3bc85
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-02-27 17:33:35 +0900

    コード修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
+  belongs_to :shipping_fee_burden
+
   extend ActiveHash::Associations::ActiveRecordExtensions
    belongs_to :category
    belongs_to :condition
