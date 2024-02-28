e30497dc7f0ce4309e3a19e1e45b11f57193c439
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-02-28 20:23:50 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -11,6 +11,11 @@ class ItemsController < ApplicationController
 
   def show
   
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   def edit
