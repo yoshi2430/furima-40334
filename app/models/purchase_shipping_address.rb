5c03df2e822f7d9076b26b7d6a85d7316f5ab7fe
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-03-04 19:58:47 +0900

    コード修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -2,10 +2,16 @@ class PurchaseShippingAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number, :token
   
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
-  validates :prefecture_id, :city, :address_line1, :phone_number, :token, :item_id, :user_id, presence: true
-  validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-  validates :prefecture_id, numericality: { only_integer: true }
+  class PurchaseShippingAddress < ApplicationRecord
+    with_options presence: true do
+      validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+      validates :prefecture_id, :city, :address_line1, :phone_number, :token, :item_id, :user_id
+    end
+  
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :prefecture_id, numericality: { only_integer: true, other_than: 1 }
+  end
+  
 
   def save(user)
     return false unless valid?
