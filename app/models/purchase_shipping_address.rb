8f2d0ef53dd6a47a417dd2bcb13fa826f4e2cfce
yoshi2430 <158545935+yoshi2430@users.noreply.github.com>
2024-03-05 16:41:24 +0900

    コード修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -3,11 +3,12 @@ class PurchaseShippingAddress
   attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :prefecture_id, :city, :address_line1, :address_line2, :phone_number, :token
   
   
-    with_options presence: true do
-      validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
-      validates :prefecture_id, :city, :address_line1, :phone_number, :token, :item_id, :user_id
-      validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-    end
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid" }
+    validates :prefecture_id, :city, :address_line1, :item_id, :user_id, presence: true
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid" }
+    validates :token, presence: { message: "can't be blank" }
+  end
   
    
     validates :prefecture_id, numericality: { only_integer: true, other_than: 1 }
